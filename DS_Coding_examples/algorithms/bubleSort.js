4fb4c6140509f324fe012bb89fc3573fa963fa0b
Yogesh Gaonkar <yogeshgaonkar@Yogeshs-MacBook-Air.local>
2023-08-05 21:47:48 +0530

    update sorting agorithms

--- DS_Coding_examples/algorithms/bubleSort.js
+++ DS_Coding_examples/algorithms/bubleSort.js
@@ -1,12 +1,12 @@
 
 
-function bubbleSort(arr){
+function bubbleSort(arr, comprFn = (a,b)=> a-b){
 
     for(let i=0;i<arr.length;i++){
         let isSwap = false;
         for(let j=0;j<arr.length-i-1;j++){
 
-            if(arr[j]>arr[j+1]){
+            if(comprFn(arr[j], arr[j+1])>0){
                 [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
                 isSwap = true;
             }
@@ -19,3 +19,44 @@ function bubbleSort(arr){
 }
 
 console.log(bubbleSort([4,2,8,6,9]));
+
+console.log(bubbleSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(bubbleSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(bubbleSort([1, 2, 3])); // [1, 2, 3]
+bubbleSort([]);
+ 
+var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+console.log(bubbleSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+ 
+var kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
+ 
+function strComp(a, b) {
+  if (a < b) { return -1;}
+  else if (a > b) { return 1;}
+  return 0;
+}
+ 
+console.log(bubbleSort(kitties, strComp)); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
+ 
+var moarKittyData = [{
+  name: "LilBub",
+  age: 7
+}, {
+  name: "Garfield",
+  age: 40
+}, {
+  name: "Heathcliff",
+  age: 45
+}, {
+  name: "Blue",
+  age: 1
+}, {
+  name: "Grumpy",
+  age: 6
+}];
+ 
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+ 
+console.log(bubbleSort(moarKittyData, oldestToYoungest));// sorted by age in descending order
--- DS_Coding_examples/algorithms/selectionSort.js
+++ DS_Coding_examples/algorithms/selectionSort.js
@@ -1,4 +1,4 @@
-function selectionSort(arr) {
+function selectionSort(arr, comprFn = (a,b)=> a-b) {
     
     for(let i=0;i<arr.length;i++){
 
@@ -6,7 +6,7 @@ function selectionSort(arr) {
         let minValue = arr[i];
         for(let j=i+1;j<arr.length;j++){
 
-            if(arr[j]<minValue){
+            if(comprFn(arr[j], minValue) < 0){
                 minValue = arr[j];
                 minIndex = j;
             }
@@ -22,3 +22,44 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([4,2,8,6,9]));
+
+console.log(selectionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(selectionSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(selectionSort([1, 2, 3])); // [1, 2, 3]
+console.log(selectionSort([]));
+ 
+var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+selectionSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+ 
+var kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
+ 
+function strComp(a, b) {
+  if (a < b) { return -1;}
+  else if (a > b) { return 1;}
+  return 0;
+}
+ 
+console.log(selectionSort(kitties, strComp)); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
+ 
+var moarKittyData = [{
+  name: "LilBub",
+  age: 7
+}, {
+  name: "Garfield",
+  age: 40
+}, {
+  name: "Heathcliff",
+  age: 45
+}, {
+  name: "Blue",
+  age: 1
+}, {
+  name: "Grumpy",
+  age: 6
+}];
+ 
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+ 
+console.log(selectionSort(moarKittyData, oldestToYoungest)); // sorted by age in descending order
